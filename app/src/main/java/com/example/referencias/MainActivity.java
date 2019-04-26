0ae118fb9df29d5a71da3c3b548d4f138f159b3e
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2019-04-26 03:34:44 -0500

    Se Agrego una imagen en el Activity

---
+++ app/src/main/java/com/example/referencias/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.referencias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
